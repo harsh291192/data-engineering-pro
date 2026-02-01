a584f86748708c3903996019883cbaaabda848a7
harsh291192 <41269694+harsh291192@users.noreply.github.com>
2026-02-01 21:11:27 +0530

    chore: setup dbt project

---
+++ dbt/models/staging/stg_posts.sql
@@ -0,0 +1,15 @@
+with source as (
+    select * from {{ source('snowflake', 'RAW_POSTS') }}
+),
+
+renamed as (
+    select
+        json_data:id::integer as post_id,
+        json_data:userId::integer as user_id,
+        json_data:title::string as title,
+        json_data:body::string as body,
+        ingested_at
+    from source
+)
+
+select * from renamed
